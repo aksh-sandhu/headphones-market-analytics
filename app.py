d4452eb346b72487953909f8979a6f2febb0e690
Aksh <sandhu2002aksh@gmail.com>
2026-08-01 13:40:14 +0530

    changed web tab header and provided developer social links

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import sidebar.market_insights as market_insights
 import sidebar.brand_analysis as brand_analysis
 
 st.set_page_config(
+  page_title="Headphones Market Analytics",
   layout = 'wide'
 )
 
@@ -24,3 +25,18 @@ elif user_option == 'Brand Analysis':
 
 elif user_option == 'About Dataset':
   about_dataset.show()
+
+
+st.sidebar.divider()
+
+st.sidebar.markdown("### 👨‍💻 Developer")
+
+st.sidebar.link_button(
+    "🔗 LinkedIn",
+    "https://www.linkedin.com/in/akshdeep-singh-sandhu-1377aa230/"
+)
+
+st.sidebar.link_button(
+    "🐙 GitHub",
+    "https://github.com/aksh-sandhu"
+)
