ab8e93bdfac14adf2829b290ca521a98dd83bb6f
Akshdeep Singh Sandhu <82952295+aksh-sandhu@users.noreply.github.com>
2026-08-03 23:00:13 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,6 +1,13 @@
+import os
 import streamlit as st
 
-HOST = st.secrets["HOST"]
-USER = st.secrets["USER"]
-PASSWORD = st.secrets["PASSWORD"]
-DATABASE = st.secrets["DATABASE"]
+try:
+    HOST = st.secrets["HOST"]
+    USER = st.secrets["USER"]
+    PASSWORD = st.secrets["PASSWORD"]
+    DATABASE = st.secrets["DATABASE"]
+except Exception:
+    HOST = os.getenv("HOST")
+    USER = os.getenv("USER")
+    PASSWORD = os.getenv("PASSWORD")
+    DATABASE = os.getenv("DATABASE")
